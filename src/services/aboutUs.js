8ed82acef7d344e8eff831fce53d4b96800f1270
Mirgubad <104448453+Mirgubad@users.noreply.github.com>
2024-01-11 23:02:16 +0400

    Update aboutUs.js

--- src/services/aboutUs.js
+++ src/services/aboutUs.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const aboutUsApi = createApi({
   reducerPath: "aboutUsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/aboutUs" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://home-decor-flax.vercel.app/aboutUs" }),
   endpoints: (builder) => ({
     getAboutUs: builder.query({
       query: () => "",
